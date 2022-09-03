3bd69aae8a4f64a21d9f27353ad7640d402be577
Kim Larsson <kim@humbleguys.se>
2022-09-03 09:42:28 +0200

    Support for changing icon

--- public/resources/views/example.blade.php
+++ public/resources/views/example.blade.php
@@ -23,6 +23,10 @@
         <x-accordion::base>
             <x-accordion::header>
                 Test
+
+                <x-slot name="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="accordion__headerIcon"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                </x-slot>
             </x-accordion::header>
     
             <x-accordion::panel>
--- src/resources/components/header.blade.php
+++ src/resources/components/header.blade.php
@@ -1,5 +1,9 @@
 <summary class="accordion__header">
     {!! $slot !!}
 
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="accordion__headerIcon"><polyline points="6 9 12 15 18 9"></polyline></svg>
+    @if (!empty($icon))
+        {!! $icon !!}
+    @else
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="accordion__headerIcon"><polyline points="6 9 12 15 18 9"></polyline></svg>
+    @endif
 </summary>
