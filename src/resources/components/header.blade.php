076d3fc7586a252305755440b69ae2f41fcb2cf4
Kim Larsson <kim@humbleguys.se>
2022-09-03 09:56:15 +0200

    Improved css

--- src/resources/components/header.blade.php
+++ src/resources/components/header.blade.php
@@ -4,6 +4,6 @@
     @if (!empty($icon))
         {!! $icon !!}
     @else
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="accordion__headerIcon"><polyline points="6 9 12 15 18 9"></polyline></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="accordion__icon"><polyline points="6 9 12 15 18 9"></polyline></svg>
     @endif
 </summary>
