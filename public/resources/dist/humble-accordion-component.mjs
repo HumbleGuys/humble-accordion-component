0f8cebd4421a20a68b16b2f34b72e12a08e80365
Kim Larsson <kim@humbleguys.se>
2023-01-11 10:57:51 +0100

    release

--- public/resources/dist/humble-accordion-component.mjs
+++ public/resources/dist/humble-accordion-component.mjs
@@ -18,7 +18,6 @@ const n = () => ({
         height: [i, t]
       },
       {
-        fill: "forwards",
         duration: this.duration,
         easing: "linear"
       }
@@ -37,13 +36,10 @@ const n = () => ({
         height: [i, t]
       },
       {
-        fill: "forwards",
         duration: this.duration,
         easing: "linear"
       }
-    ), this.animation.onfinish = () => setTimeout(() => {
-      this.onAnimationFinish(!0);
-    }, 100), this.animation.oncancel = () => this.isExpanding = !1;
+    ), this.animation.onfinish = () => window.requestAnimationFrame(() => this.onAnimationFinish(!0)), this.animation.oncancel = () => this.isExpanding = !1;
   },
   onAnimationFinish(i) {
     this.$el.open = i, this.isOpen = i, this.animation = null, this.isClosing = !1, this.isExpanding = !1, this.$el.style.height = "", this.$el.style.overflow = "";
